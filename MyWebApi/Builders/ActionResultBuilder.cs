22e2ec7f4063203d7568ec33af6a0330d32cfcc7
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 15:28:19 +0300

    Added empty IActionResultBuilder with default constructor accepting action name and action result

---
+++ MyWebApi/Builders/ActionResultBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWebApi.Builders
+{
+    using Contracts;
+
+    public class ActionResultBuilder<TActionResult> : IActionResultBuilder<TActionResult>
+    {
+        private string actionName;
+        private TActionResult actionResult;
+
+        public ActionResultBuilder(string actionName, TActionResult actionResult)
+        {
+            this.actionName = actionName;
+            this.actionResult = actionResult;
+        }
+    }
+}
