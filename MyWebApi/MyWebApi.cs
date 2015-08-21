4d592d7d18d2687ec9680525426140f6643e8f2e
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 14:46:06 +0300

    Added initial start up Controller methods in MyWebApi class

--- MyWebApi/MyWebApi.cs
+++ MyWebApi/MyWebApi.cs
@@ -1,7 +1,20 @@
 ﻿namespace MyWebApi
 {
+    using System;
+    using System.Web.Http;
+
     public static class MyWebApi
     {
-        
+        public static void Controller<TController>()
+            where TController : ApiController
+        {
+
+        }
+
+        public static void Controller<TController>(Func<TController> construction)
+            where TController : ApiController
+        {
+
+        }
     }
 }
