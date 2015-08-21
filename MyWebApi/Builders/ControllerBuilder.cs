45c4feb6db65649210fc809715946692263ae5dc
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 14:57:37 +0300

    Added ControllerBuilder.cs

---
+++ MyWebApi/Builders/ControllerBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace MyWebApi.Builders
+{
+    using System.Web.Http;
+
+    using Contracts;
+
+    public class ControllerBuilder<TController> : IControllerBuilder<TController>
+        where TController : ApiController
+    {
+        private TController controller;
+
+        public ControllerBuilder(TController controllerInstance)
+        {
+            this.controller = controllerInstance;
+        }
+
+        public void Calling<TAction>()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
