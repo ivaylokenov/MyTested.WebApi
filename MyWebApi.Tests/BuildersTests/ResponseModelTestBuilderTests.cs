a9446610dead964d3a52590dfa0a9db243939cf0
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 21:42:20 +0300

    Added ResponseModelTestBuilderTests

---
+++ MyWebApi.Tests/BuildersTests/ResponseModelTestBuilderTests.cs
@@ -0,0 +1,54 @@
+﻿namespace MyWebApi.Tests.BuildersTests
+{
+    using System.Collections.Generic;
+
+    using ControllerSetups;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ResponseModelTestBuilderTests
+    {
+        [Test]
+        public void WithResponseModelShouldNotThrowExceptionWithCorrectResponseModel()
+        {
+            MyWebApi
+                .Controller<WebApiController>()
+                .Calling(c => c.OkResultWithResponse())
+                .ShouldReturnOkResult()
+                .WithResponseModel<ICollection<ResponseModel>>();
+        }
+
+        [Test]
+        public void WithResponseShouldNotThrowExceptionWithIncorrectInheritedTypeArgument()
+        {
+            MyWebApi
+                .Controller<WebApiController>()
+                .Calling(c => c.OkResultWithResponse())
+                .ShouldReturnOkResult()
+                .WithResponseModel<IList<ResponseModel>>();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ResponseModelAssertionException))]
+        public void WithResponseShouldThrowExceptionWithIncorrectResponseModel()
+        {
+            MyWebApi
+                .Controller<WebApiController>()
+                .Calling(c => c.OkResultWithResponse())
+                .ShouldReturnOkResult()
+                .WithResponseModel<ResponseModel>();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ResponseModelAssertionException))]
+        public void WithResponseShouldThrowExceptionWithIncorrectGenericTypeArgument()
+        {
+            MyWebApi
+                .Controller<WebApiController>()
+                .Calling(c => c.OkResultWithResponse())
+                .ShouldReturnOkResult()
+                .WithResponseModel<ICollection<int>>();
+        }
+    }
+}
