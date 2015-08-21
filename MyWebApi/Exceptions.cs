9f0e0324bd1e38c21d2b16e044bcaa189bd168de
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 18:15:55 +0300

    Added Exceptions file for all assertion exceptions

---
+++ MyWebApi/Exceptions.cs
@@ -0,0 +1,19 @@
+﻿namespace MyWebApi
+{
+    using System;
+
+    /// <summary>
+    /// Exception for invalid action return type when expecting IHttpActionResult.
+    /// </summary>
+    public class IHttpActionResultAssertionException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the IHttpActionResultAssertionException class.
+        /// </summary>
+        /// <param name="message">Message required by System.Exception class.</param>
+        public IHttpActionResultAssertionException(string message)
+            : base(message)
+        {
+        }
+    }
+}
