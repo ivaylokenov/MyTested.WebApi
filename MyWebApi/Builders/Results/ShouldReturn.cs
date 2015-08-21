efb3b197ee9d19d49535b88e77f11aa9b544876c
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 21:40:51 +0300

    ShouldReturn now allows non-generic by type checking

--- MyWebApi/Builders/Results/ShouldReturn.cs
+++ MyWebApi/Builders/Results/ShouldReturn.cs
@@ -1,11 +1,18 @@
 ﻿namespace MyWebApi.Builders.Results
 {
+    using System;
+
     /// <summary>
     /// Class containing methods for testing return type.
     /// </summary>
     /// <typeparam name="TActionResult">Result from invoked action in ASP.NET Web API controller.</typeparam>
     public partial class ActionResultTestBuilder<TActionResult>
     {
+        public void ShouldReturn(Type returnType)
+        {
+            this.ValidateActionReturnType(returnType, true);
+        }
+
         /// <summary>
         /// Tests whether action result is of the provided generic type.
         /// </summary>
