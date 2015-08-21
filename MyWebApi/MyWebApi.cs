f9d358b140934c4a5c838884cbb3f62eb85608aa
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 15:02:03 +0300

    Added documentation summary in MyWebApi methods

--- MyWebApi/MyWebApi.cs
+++ MyWebApi/MyWebApi.cs
@@ -2,11 +2,17 @@
 {
     using System;
     using System.Web.Http;
+
     using Builders;
     using Builders.Contracts;
 
     public static class MyWebApi
     {
+        /// <summary>
+        /// Selects controller on which the test will be executed. Controller is instantiated with default constructor
+        /// </summary>
+        /// <typeparam name="TController">Controller of type ApiController</typeparam>
+        /// <returns>Controller builder used to build the test case</returns>
         public static IControllerBuilder<TController> Controller<TController>()
             where TController : ApiController
         {
@@ -14,6 +20,12 @@
             return new ControllerBuilder<TController>(controllerInstance);
         }
 
+        /// <summary>
+        /// Selects controller on which the test will be executed. Controller is instantiated using construction function
+        /// </summary>
+        /// <typeparam name="TController">Controller of type ApiController</typeparam>
+        /// <param name="construction">Construction function returning the instantiated controller</param>
+        /// <returns>Controller builder used to build the test case</returns>
         public static IControllerBuilder<TController> Controller<TController>(Func<TController> construction)
             where TController : ApiController
         {
