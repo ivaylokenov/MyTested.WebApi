2400a108c6dd97ea32334782a7e75643451f3f40
apxoht <ivaylo.kenov@gmail.com>
2015-08-21 17:02:07 +0300

    Added class Validator for validating common invalid data

---
+++ MyWebApi/Utilities/Validator.cs
@@ -0,0 +1,29 @@
+﻿namespace MyWebApi.Utilities
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void CheckForNullReference(
+            object value,
+            string parameterName = "value",
+            string errorMessageName = "Value")
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName, string.Format("{0} cannot be null", errorMessageName));
+            }
+        }
+
+        public static void CheckForNotEmptyString(
+            string value,
+            string parameterName = "value",
+            string errorMessageName = "Value")
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException(parameterName, string.Format("{0} cannot be null or empty", errorMessageName));
+            }
+        }
+    }
+}
