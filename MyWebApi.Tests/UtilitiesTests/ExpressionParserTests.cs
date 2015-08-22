910ea45a888fbb513429a26e35a7e208e7e1bfea
apxoht <ivaylo.kenov@gmail.com>
2015-08-22 11:41:58 +0300

    Added ExpressionParserTests for ResolveMethodArguments from expression (closes #8)

--- MyWebApi.Tests/UtilitiesTests/ExpressionParserTests.cs
+++ MyWebApi.Tests/UtilitiesTests/ExpressionParserTests.cs
@@ -1,9 +1,12 @@
 ﻿namespace MyWebApi.Tests.UtilitiesTests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
     using System.Web.Http;
 
+    using Common;
     using ControllerSetups;
 
     using NUnit.Framework;
@@ -27,6 +30,41 @@
         {
             Expression<Func<int>> expression = () => 0;
             ExpressionParser.GetMethodName(expression);
+        }
+
+        [Test]
+        public void ResolveMethodArgumentsShouldReturnCorrectCollectionOfTypeValuePair()
+        {
+            var requestModel = new RequestModel
+            {
+                Id = 2,
+                Name = "Test"
+            };
+
+            Expression<Func<WebApiController, IHttpActionResult>> expression = c => c.OkResultActionWithRequestBody(1, requestModel);
+
+            var result = ExpressionParser.ResolveMethodArguments(expression);
+
+            var typeValuePairs = result as IList<TypeValuePair> ?? result.ToArray();
+            var firstArgument = typeValuePairs.First();
+            var secondArgument = typeValuePairs.Last();
+            var secondArgumentAsRequestModel = secondArgument.Value as RequestModel;
+
+            Assert.AreEqual(typeof(int), firstArgument.Type);
+            Assert.AreEqual(1, firstArgument.Value);
+
+            Assert.AreEqual(typeof(RequestModel), secondArgument.Type);
+            Assert.IsNotNull(secondArgumentAsRequestModel);
+            Assert.AreEqual(2, secondArgumentAsRequestModel.Id);
+            Assert.AreEqual("Test", secondArgumentAsRequestModel.Name);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ResolveMethodArgumentsShouldThrowArgumentExceptionWithInvalidMethodCallExpression()
+        {
+            Expression<Func<int>> expression = () => 0;
+            ExpressionParser.ResolveMethodArguments(expression);
         }
     }
 }
