7041ae0a750812da64ad01f6802b60193359e471
apxoht <ivaylo.kenov@gmail.com>
2015-08-22 11:41:37 +0300

    Added ResolveMethodArguments in ExpressionParser (#8)

--- MyWebApi/Utilities/ExpressionParser.cs
+++ MyWebApi/Utilities/ExpressionParser.cs
@@ -1,7 +1,11 @@
 ﻿namespace MyWebApi.Utilities
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
+    using Common;
 
     /// <summary>
     /// Utility class helping parsing expression trees.
@@ -15,13 +19,32 @@
         /// <returns>Method name as string.</returns>
         public static string GetMethodName(LambdaExpression expression)
         {
+            var methodCallExpression = GetMethodCallExpresstion(expression);
+            return methodCallExpression.Method.Name;
+        }
+
+        public static IEnumerable<TypeValuePair> ResolveMethodArguments(LambdaExpression expression)
+        {
+            var methodCallExpression = GetMethodCallExpresstion(expression);
+            return methodCallExpression.Arguments
+                .Select(argument => Expression.Lambda(argument).Compile().DynamicInvoke())
+                .Select(value => new TypeValuePair
+                {
+                    Type = value.GetType(),
+                    Value = value
+                })
+                .ToList();
+        }
+
+        private static MethodCallExpression GetMethodCallExpresstion(LambdaExpression expression)
+        {
             var methodCallExpression = expression.Body as MethodCallExpression;
             if (methodCallExpression == null)
             {
                 throw new ArgumentException("Provided expression is not a valid method call");
             }
 
-            return methodCallExpression.Method.Name;
+            return methodCallExpression;
         }
     }
 }
