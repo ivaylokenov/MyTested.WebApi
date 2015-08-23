fc8fb97bc0efa1d56ba2c338bc4bb32508dbb4fa
apxoht <ivaylo.kenov@gmail.com>
2015-08-24 01:37:03 +0300

    Added IUserBuilder for building custom IPrinciple object (#15)

---
+++ MyWebApi/Builders/Contracts/IUserBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace MyWebApi.Builders.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IUserBuilder
+    {
+        IUserBuilder WithUsername(string username);
+
+        IUserBuilder WithAuthenticationType(string authenticationType);
+
+        IUserBuilder InRole(string role);
+
+        IUserBuilder InRoles(IEnumerable<string> roles);
+    }
+}
---
+++ MyWebApi/Builders/UserBuilder.cs
@@ -0,0 +1,51 @@
+﻿namespace MyWebApi.Builders
+{
+    using System.Collections.Generic;
+    using System.Security.Principal;
+
+    using Common.Extensions;
+    using Common.Identity;
+
+    using Contracts;
+
+    public class UserBuilder : IUserBuilder
+    {
+        private string constructedUsername;
+        private string constructedAuthenticationType;
+        private readonly ICollection<string> constructedRoles;
+
+        public UserBuilder()
+        {
+            this.constructedRoles = new HashSet<string>();
+        }
+
+        public IUserBuilder WithUsername(string username)
+        {
+            this.constructedUsername = username;
+            return this;
+        }
+
+        public IUserBuilder WithAuthenticationType(string authenticationType)
+        {
+            this.constructedAuthenticationType = authenticationType;
+            return this;
+        }
+
+        public IUserBuilder InRole(string role)
+        {
+            this.constructedRoles.Add(role);
+            return this;
+        }
+
+        public IUserBuilder InRoles(IEnumerable<string> roles)
+        {
+            roles.ForEach(role => this.constructedRoles.Add(role));
+            return this;
+        }
+
+        internal IPrincipal GetUser()
+        {
+            return new MockedIPrinciple(constructedUsername, constructedAuthenticationType, constructedRoles);
+        }
+    }
+}
