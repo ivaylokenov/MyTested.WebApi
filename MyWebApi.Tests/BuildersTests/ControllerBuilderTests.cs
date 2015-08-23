33fd5f8dbfdb818af02b13ed415cfd96ab4a4be8
apxoht <ivaylo.kenov@gmail.com>
2015-08-24 01:08:24 +0300

    Added tests for unauthorized and default authorized controller

--- MyWebApi.Tests/BuildersTests/ControllerBuilderTests.cs
+++ MyWebApi.Tests/BuildersTests/ControllerBuilderTests.cs
@@ -64,6 +64,43 @@
             Assert.AreEqual(0, modelState.Keys.Count);
         }
 
+        [Test]
+        public void WithAuthorizedUserShouldPopulateUserPropertyWithDefaultValues()
+        {
+            var controllerBuilder = MyWebApi
+                .Controller<WebApiController>()
+                .WithAuthorizedUser();
+
+            controllerBuilder
+                .Calling(c => c.Authorized())
+                .ShouldReturnOk();
+
+            var controllerUser = controllerBuilder.Controller.User;
+
+            Assert.AreEqual(false, controllerUser.IsInRole("Any"));
+            Assert.AreEqual("TestUser", controllerUser.Identity.Name);
+            Assert.AreEqual("Passport", controllerUser.Identity.AuthenticationType);
+            Assert.AreEqual(true, controllerUser.Identity.IsAuthenticated);
+        }
+
+        [Test]
+        public void WithAuthorizedNotCalledShouldNotHaveAuthorizedUser()
+        {
+            var controllerBuilder = MyWebApi
+                .Controller<WebApiController>();
+
+            controllerBuilder
+                .Calling(c => c.Authorized())
+                .ShouldReturnNotFound();
+
+            var controllerUser = controllerBuilder.Controller.User;
+
+            Assert.AreEqual(false, controllerUser.IsInRole("Any"));
+            Assert.AreEqual(null, controllerUser.Identity.Name);
+            Assert.AreEqual(null, controllerUser.Identity.AuthenticationType);
+            Assert.AreEqual(false, controllerUser.Identity.IsAuthenticated);
+        }
+
         private void CheckActionResultTestBuilder<TActionResult>(
             IActionResultTestBuilder<TActionResult> actionResultTestBuilder,
             string expectedActionName)
