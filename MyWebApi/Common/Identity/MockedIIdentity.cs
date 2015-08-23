da82713e8690d76259993ada59d1e159decf4d42
apxoht <ivaylo.kenov@gmail.com>
2015-08-24 01:03:12 +0300

    Added MockedIIdentity and MockedIPrinciple (#15)

---
+++ MyWebApi/Common/Identity/MockedIIdentity.cs
@@ -0,0 +1,24 @@
+﻿namespace MyWebApi.Common.Identity
+{
+    using System.Security.Principal;
+
+    public class MockedIIdentity : IIdentity
+    {
+        private readonly string name;
+        private readonly string authenticationType;
+        private readonly bool isAuthenticated;
+
+        public MockedIIdentity(string name = null, string authenticationType = null, bool isAuthenticated = false)
+        {
+            this.name = name;
+            this.authenticationType = authenticationType;
+            this.isAuthenticated = isAuthenticated;
+        }
+
+        public string Name { get { return name; } }
+
+        public string AuthenticationType { get { return authenticationType; } }
+
+        public bool IsAuthenticated { get { return isAuthenticated; } }
+    }
+}
---
+++ MyWebApi/Common/Identity/MockedIPrinciple.cs
@@ -0,0 +1,31 @@
+﻿namespace MyWebApi.Common.Identity
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Principal;
+
+    public class MockedIPrinciple : IPrincipal
+    {
+        private readonly IEnumerable<string> roles;
+
+        public MockedIPrinciple()
+        {
+            this.roles = new HashSet<string>();
+        }
+
+        public static IPrincipal CreateUnauthorized()
+        {
+            return new MockedIPrinciple
+            {
+                Identity = new MockedIIdentity()
+            };
+        }
+
+        public bool IsInRole(string role)
+        {
+            return this.roles.Contains(role);
+        }
+
+        public IIdentity Identity { get; private set; }
+    }
+}
