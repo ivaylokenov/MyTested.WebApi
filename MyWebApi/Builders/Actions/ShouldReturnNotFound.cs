b847bf822fd154a0c3a96a067dca912f79ac7f70
apxoht <ivaylo.kenov@gmail.com>
2015-08-24 00:06:41 +0300

    Added ShouldReturnNotFound (#23)

---
+++ MyWebApi/Builders/Actions/ShouldReturnNotFound.cs
@@ -0,0 +1,19 @@
+﻿namespace MyWebApi.Builders.Actions
+{
+    using System.Web.Http.Results;
+
+    /// <summary>
+    /// Class containing methods for testing NotFoundResult.
+    /// </summary>
+    /// <typeparam name="TActionResult">Result from invoked action in ASP.NET Web API controller.</typeparam>
+    public partial class ActionResultTestBuilder<TActionResult>
+    {
+        /// <summary>
+        /// Tests whether action result is NotFoundResult.
+        /// </summary>
+        public void ShouldReturnNotFound()
+        {
+            this.ShouldReturn<NotFoundResult>();
+        }
+    }
+}
