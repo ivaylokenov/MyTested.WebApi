c78767742ded3eae76e2db9201dc0f5079807900
apxoht <ivaylo.kenov@gmail.com>
2015-08-25 15:42:50 +0300

    Updated AssemblyInfo

--- MyWebApi.Tests/Properties/AssemblyInfo.cs
+++ MyWebApi.Tests/Properties/AssemblyInfo.cs
@@ -1,35 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MyWebApi.Tests")]
 [assembly: AssemblyDescription("Unit tests for ASP.NET Web API Fluent Testing Framework")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ivaylo Kenov")]
 [assembly: AssemblyProduct("MyWebApi.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("29f6a15e-c6d1-4c93-83c8-18ff839950e8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("0.0.1.0")]
