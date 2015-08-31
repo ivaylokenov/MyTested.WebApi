a076dda0f7d53306468f547fbf34fee2bb8db750
apxoht <ivaylo.kenov@gmail.com>
2015-08-31 20:34:05 +0300

    Implemented ExceptionMessageTestBuilder (#42)

--- MyWebApi/Builders/ExceptionErrors/ExceptionMessageTestBuilder.cs
+++ MyWebApi/Builders/ExceptionErrors/ExceptionMessageTestBuilder.cs
@@ -2,13 +2,15 @@
 {
     using System.Web.Http;
     using Base;
+    using Common.Extensions;
     using Contracts.ExceptionErrors;
+    using Exceptions;
 
     public class ExceptionMessageTestBuilder
         : BaseTestBuilder, IExceptionMessageTestBuilder
     {
         private readonly IExceptionTestBuilder exceptionTestBuilder;
-        private string actualMessage;
+        private readonly string actualMessage;
 
         public ExceptionMessageTestBuilder(
             ApiController controller,
@@ -23,27 +25,65 @@
 
         public IExceptionTestBuilder ThatEquals(string errorMessage)
         {
-            throw new System.NotImplementedException();
+            if (this.actualMessage != errorMessage)
+            {
+                this.ThrowNewInvalidExceptionAssertionException(
+                    "When calling {0} action in {1} expected exception message to be '{2}', but instead found '{3}'.",
+                    errorMessage);
+            }
+
+            return this.exceptionTestBuilder;
         }
 
         public IExceptionTestBuilder BeginningWith(string beginMessage)
         {
-            throw new System.NotImplementedException();
+            if (!this.actualMessage.StartsWith(beginMessage))
+            {
+                this.ThrowNewInvalidExceptionAssertionException(
+                    "When calling {0} action in {1} expected exception message to begin with '{2}', but instead found '{3}'.",
+                    beginMessage);
+            }
+
+            return this.exceptionTestBuilder;
         }
 
         public IExceptionTestBuilder EndingWith(string endMessage)
         {
-            throw new System.NotImplementedException();
+            if (!this.actualMessage.EndsWith(endMessage))
+            {
+                this.ThrowNewInvalidExceptionAssertionException(
+                    "When calling {0} action in {1} expected exception message to end with '{2}', but instead found '{3}'.",
+                    endMessage);
+            }
+
+            return this.exceptionTestBuilder;
         }
 
         public IExceptionTestBuilder Containing(string containsMessage)
         {
-            throw new System.NotImplementedException();
+            if (!this.actualMessage.Contains(containsMessage))
+            {
+                this.ThrowNewInvalidExceptionAssertionException(
+                    "When calling {0} action in {1} expected exception message to contain '{2}', but instead found '{3}'.",
+                    containsMessage);
+            }
+
+            return this.exceptionTestBuilder;
         }
 
         public IExceptionTestBuilder AndAlso()
         {
             return this.exceptionTestBuilder;
+        }
+
+        private void ThrowNewInvalidExceptionAssertionException(string messageFormat, string operation)
+        {
+            throw new InvalidExceptionAssertionException(string.Format(
+                messageFormat,
+                this.ActionName,
+                this.Controller.GetName(),
+                operation,
+                this.actualMessage));
         }
     }
 }
