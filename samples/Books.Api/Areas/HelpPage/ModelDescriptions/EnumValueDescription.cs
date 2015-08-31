4caa468d75df62ad424e44cea6c78e6ce3598f4a
apxoht <ivaylo.kenov@gmail.com>
2015-09-01 00:29:13 +0300

    Added initial projects for sample solution

---
+++ samples/Books.Api/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Books.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ samples/Books.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Books.Api.WebApiApplication" Language="C#" %>
