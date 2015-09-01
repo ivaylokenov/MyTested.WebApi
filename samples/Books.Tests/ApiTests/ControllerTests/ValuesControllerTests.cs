bee2daa805064768e355ef26dd3a41ed63fba4a8
apxoht <ivaylo.kenov@gmail.com>
2015-09-01 13:09:42 +0300

    Updated README.md and tested NuGet package in Sample (#38)

---
+++ samples/Books.Tests/ApiTests/ControllerTests/ValuesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Books.Tests.ApiTests.ControllerTests
+{
+    using System.Collections.Generic;
+    using Api.Controllers;
+    using MyWebApi;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ValuesControllerTests
+    {
+        [Test]
+        public void ValuesControllerShouldTest()
+        {
+            MyWebApi
+                .Controller<ValuesController>()
+                .Calling(c => c.Get())
+                .ShouldReturn<IEnumerable<string>>();
+        }
+    }
+}
