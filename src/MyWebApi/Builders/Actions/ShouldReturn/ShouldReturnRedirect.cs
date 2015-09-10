919ee529eef2e6f00fb1bd4ce23be80ef3855beb
apxoht <ivaylo.kenov@gmail.com>
2015-09-10 22:09:49 +0300

    Added ShouldReturnRedirect (#47)

---
+++ src/MyWebApi/Builders/Actions/ShouldReturn/ShouldReturnRedirect.cs
@@ -0,0 +1,22 @@
+﻿namespace MyWebApi.Builders.Actions.ShouldReturn
+{
+    using System.Web.Http.Results;
+
+    /// <summary>
+    /// Class containing methods for testing RedirectResult or RedirectToRouteResult.
+    /// </summary>
+    /// <typeparam name="TActionResult">Result from invoked action in ASP.NET Web API controller.</typeparam>
+    public partial class ShouldReturnTestBuilder<TActionResult>
+    {
+        public void Redirect()
+        {
+            var actionResultAsRedirectResult = this.ActionResult as RedirectResult;
+            if (actionResultAsRedirectResult != null)
+            {
+                
+            }
+
+
+        }
+    }
+}
